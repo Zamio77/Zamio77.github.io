f50f7d870a1ce50f30aba7d8db913dbc71920027
Zamio77 <38842486+Zamio77@users.noreply.github.com>
2019-04-03 19:39:53 -0400

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -3,19 +3,8 @@ let fahUnit = "F";
 let celUnit = "C";
 let units = "imperial";
 let searchButton = document.getElementById("search-btn");
-searchButton.addEventListener("click", getWeather);
 let tempButton = document.getElementById("tempunit");
-tempButton.addEventListener("click", function() {
-  if (tempButton.innerHTML === "F") {
-    units = "metric";
-    tempButton.innerHTML = celUnit;
-    getWeather();
-  } else if (tempButton.innerHTML === "C") {
-    units = "imperial";
-    tempButton.innerHTML = fahUnit;
-    getWeather();
-  }
-});
+
 
 let icon_src = "./Icons/animated/day.svg";
 let icon_element = document.getElementById("icon");
@@ -24,14 +13,14 @@ let searchText = document.getElementById("search-txt");
 
 function getWeather() {
   return fetch(
-    // Chrome requires HTTPS connections but openweather api, the free version, comes through as HTTP
-    // added 'https://crossorigin.me/ to the api call to fix.
-    `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?zip=${
+      // Chrome requires HTTPS connections but openweather api, the free version, comes through as HTTP
+      // added 'https://crossorigin.me/ to the api call to fix.
+      `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?zip=${
       searchText.value
     }&units=${units}&appid=3c9639b2d937cc613e77cf142beccbe4`
-  )
+    )
     .then(response => response.json())
-    .then(function(data) {
+    .then(function (data) {
       // Begin accessing JSON data here
       let location = data.name + ", " + data.sys.country;
       document.getElementById("location").innerHTML = location;
@@ -43,3 +32,20 @@ function getWeather() {
     })
     .catch(error => console.error(`Error: ${error}`));
 }
+
+// Set the actionlistener for the searchButton
+searchButton.addEventListener("click", getWeather);
+
+// Setup the TempButton
+tempButton.addEventListener("click", function () {
+  if (tempButton.innerHTML === "F") {
+    units = "metric";
+    tempButton.innerHTML = celUnit;
+    getWeather();
+  } else // (tempButton.innerHTML === "C") 
+  {
+    units = "imperial";
+    tempButton.innerHTML = fahUnit;
+    getWeather();
+  }
+});
