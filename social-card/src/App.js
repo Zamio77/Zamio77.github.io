770c67034909bbb2dd2ad9e78c4c7bd790d24fdb
Zamio77 <38842486+Zamio77@users.noreply.github.com>
2019-12-09 23:19:45 -0500

    Social-card being added
    Social-card being added to the app

---
+++ social-card/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './AdBox'
+import AdBox from './AdBox';
+
+function App() {
+  return (
+    <div className="App">
+      <AdBox />
+    </div>
+  );
+}
+
+export default App;
