56821cf578d81a59129d3b3e3316b1ece0a07015
Zamio77 <38842486+Zamio77@users.noreply.github.com>
2019-12-10 23:19:12 -0500

    Formatting Cards
    Adding formatting to the cards

--- social-card/src/Cards/Cards.js
+++ social-card/src/Cards/Cards.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
 import Card from './Card'
 import img1 from '../assets/36539-2-chrono-trigger-photos.png'
-import chatImg from '../assets/'
+import chatImg from '../assets/chat.png'
+import Text from './Text'
 
 class Cards extends Component {
     render() {
         return (
             <div className='container-fluid d-flex justify-content-center'>
-            <Card imgsrc={img1} title='Chrono Trigger' txt='A classic game for SNES, Chrono Trigger remains
-                                        fantastic and is consistently rated
-                                        one of the greatest games of all time!'
-                                        />
+                <Card 
+                    imgsrc={img1} 
+                    title='Chrono Trigger' 
+                    txt='A classic game for SNES, Chrono Trigger 
+                    remains fantastic and is consistently rated
+                    one of the greatest games of all time!'
+                    chat='Like'
+                />
             </div>
         )
     }
---
+++ social-card/src/Cards/Text.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+let Text = {
+    chronTrigger: 'A classic game for SNES, Chrono Trigger remains fantastic and is consistently rated one of the greatest games of all time!'
+}
+
+export default Text
