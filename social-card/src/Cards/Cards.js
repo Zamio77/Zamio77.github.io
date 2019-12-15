e3eb9c3f6b3767e8451582a11454eec26f68d057
Zamio77 <38842486+Zamio77@users.noreply.github.com>
2019-12-15 15:55:27 -0500

    Styling and map function
    Added styling and a map function to load the card objects to the cards.

--- social-card/src/Cards/Cards.js
+++ social-card/src/Cards/Cards.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react'
 import Card from './Card'
 import game from './Text'
 
-class Cards extends Component {
-    render() {
-        return (
-            <div className='container-fluid d-flex justify-content-center'>
+function Cards(props) {
+   return ( <div>
+        { game.map((props) => {
+           return <div className='container-fluid d-flex justify-content-center'>
                 <Card 
-                    imgsrc={game.photo} 
-                    title={game.title} 
-                    txt={game.chronTrigger}
+                    imgsrc={props.photo} 
+                    title={props.title} 
+                    txt={props.msg}
                 />
             </div>
+        }
         )
-    }
+    } 
+    </div>
+    )
 }
+
 
 export default Cards
--- social-card/src/Cards/Text.js
+++ social-card/src/Cards/Text.js
@@ -1,9 +1,16 @@
 import img1 from '../assets/36539-2-chrono-trigger-photos.png'
+import img2 from '../assets/FinalFantasy.jpeg'
 
-let Text = {
-    chronTrigger: 'A classic game for SNES, Chrono Trigger remains fantastic and is consistently rated one of the greatest games of all time!',
-    photo: img1,
-    title: 'Chrono Trigger'
+let Text = [{
+    title: 'Chrono Trigger',
+    msg: 'A classic game for SNES, Chrono Trigger remains fantastic and is consistently rated one of the greatest games of all time!',
+    photo: img1
+},
+{
+    tile: 'Final Fantasy',
+    msg: 'A classic game for the Playstation. Originally released in 1997, it has been widely regarded as one of the greatest games of all time, ranking #1 on IGN list of best games!',
+    photo: img2  
 }
+]
 
 export default Text
