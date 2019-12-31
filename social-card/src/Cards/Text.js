9adbf1d15a7fb4b75dd3bba243386e5b38b5b5b0
Zamio77 <38842486+Zamio77@users.noreply.github.com>
2019-12-30 22:02:13 -0500

    Finalizing Social Card App
    Finalizing social card app with react

--- social-card/src/Cards/Text.js
+++ social-card/src/Cards/Text.js
@@ -1,5 +1,6 @@
 import img1 from '../assets/36539-2-chrono-trigger-photos.png'
 import img2 from '../assets/FinalFantasy.jpeg'
+import img3 from '../assets/legend-of-zelda-nes.jpg'
 
 let Text = [{
     title: 'Chrono Trigger',
@@ -7,9 +8,14 @@ let Text = [{
     photo: img1
 },
 {
-    tile: 'Final Fantasy',
+    title: 'Final Fantasy',
     msg: 'A classic game for the Playstation. Originally released in 1997, it has been widely regarded as one of the greatest games of all time, ranking #1 on IGN list of best games!',
     photo: img2  
+},
+{
+    title: 'The Legend of Zelda',
+    msg: 'A classic game for the NES. One of the first games that used save software. Greatest game of all time',
+    photo: img3
 }
 ]
 
