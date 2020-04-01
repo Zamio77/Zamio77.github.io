6d11d214ea4d436d1dff68a34d3de924b6e7288a
Zamio77 <38842486+Zamio77@users.noreply.github.com>
2020-04-01 00:34:32 -0400

    Update server.js

--- weatherJournalApp/weather-journal-app/server.js
+++ weatherJournalApp/weather-journal-app/server.js
@@ -3,8 +3,13 @@ projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
+
 // Start up an instance of app
 const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser');
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({
@@ -15,9 +20,39 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
+
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
 const port = 3000;
+
+// Spin up the server
+const server = app.listen(port, listening);
+
+// Callback to degug
+function listening() {
+    console.log('server running');
+    console.log(`running on localhost ${port}`);
+}
+
+// Callback function to complete GET '/all'
+app.get('/all', getData);
+
+const getData = (req, res) => {
+    res.send(projectData);
+    console.log(projectData);
+}
+
+app.post('/add', info);
+
+const info = (req, res) => {
+
+    projectData.temp = req.body.temp;
+    projectData.date = req.body.date;
+    projectData.content = req.body.content;
+
+    res.send(projectData);
+    console.log(projectData);
+}
