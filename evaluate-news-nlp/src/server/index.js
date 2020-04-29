fd454fc6dd7416ae94c50eac6900b9e4c73bf821
Doug Lamar <38842486+Zamio77@users.noreply.github.com>
2020-04-28 22:56:11 -0400

    Update index.js

--- evaluate-news-nlp/src/server/index.js
+++ evaluate-news-nlp/src/server/index.js
@@ -10,7 +10,7 @@ console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile(path.resolve('dist/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
