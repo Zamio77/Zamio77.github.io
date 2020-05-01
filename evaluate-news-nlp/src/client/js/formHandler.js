b3b0ca6051dcc2defdc10a61cb9448eefbc37aa4
Doug Lamar <38842486+Zamio77@users.noreply.github.com>
2020-05-01 13:47:27 -0400

    Reorganizing the formHandler file

--- evaluate-news-nlp/src/client/js/formHandler.js
+++ evaluate-news-nlp/src/client/js/formHandler.js
@@ -12,19 +12,17 @@ function handleSubmit(event) {
         document.getElementById('results').innerHTML = res.message
     })
 
-    const getApi = async () => {
-        const request = await fetch("http://localhost:8080/api");
-        try {
-          const allData = await request.json();
-          console.log(allData);
-        } catch (error) {
-          console.log("error", error);
-        }
-      };
-
     getApi();
 }
 
-
+const getApi = async () => {
+    const request = await fetch("http://localhost:8080/api");
+    try {
+      const allData = await request.json();
+      console.log(allData);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
 export { handleSubmit }
