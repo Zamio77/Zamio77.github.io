38304dcd792d52d5b88b42a239f5e38ead4aa5a3
Doug Lamar <38842486+Zamio77@users.noreply.github.com>
2020-05-02 23:31:14 -0400

    Update formHandler.js

--- evaluate-news-nlp/src/client/js/formHandler.js
+++ evaluate-news-nlp/src/client/js/formHandler.js
@@ -12,7 +12,24 @@ function handleSubmit(event) {
     //     document.getElementById('results').innerHTML = res.message
     // })
 
+<<<<<<< HEAD
     Client.getApi('/api', {URL: formText});
 }
 
+=======
+    const getApi = async () => {
+        const request = await fetch("http://localhost:8080/api");
+        try {
+          const allData = await request.json();
+          console.log(allData);
+        } catch (error) {
+          console.log("error", error);
+        }
+      };
+
+    getApi();
+}
+
+
+>>>>>>> parent of b3b0ca60... Reorganizing the formHandler file
 
